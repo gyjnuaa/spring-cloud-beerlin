1e84f2aac3ff9d6bc0d67b0dbd155f4c59fbbaaf
gaoyanjing <gaoyanjing@youbangsoft.com>
2017-11-20 20:19:18 +0800

    spring-cloud-sleuth  zipkin链路追踪

---
+++ customer/src/main/java/com/example/demo/ZipkinDemo/ZipkinController.java
@@ -0,0 +1,18 @@
+package com.example.demo.ZipkinDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GYJ on 2017-11-20.
+ */
+@RestController
+@RequestMapping("/Zipkin")
+public class ZipkinController {
+    @RequestMapping(value = "/getPrint",method = RequestMethod.GET)
+    public String  getPrint(){
+        System.out.print("Hello ZipKin!!");
+        return "Hello ZipKin!!";
+    }
+}
